185a465a424ede71ebd6406ffe38b283326ce523
MuckT <mcgtom10@gmail.com>
2021-03-22 00:47:14 -0700

    feat: add stretch goal to use a loop to display beasts

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -4,9 +4,9 @@ class HornedBeasts extends React.Component {
   render() {
     return (
       <div>
-        <h2>{this.name}</h2>
-        <img title={this.name} alt={this.name + "image"} src={this.imgUrl}></img>
-        <p>{this.description}</p>
+        <h2>{this.props.name}</h2>
+        <img title={this.props.name} alt={this.props.name + " image"} src={this.props.imgUrl} ></img>
+        <p>{this.props.description}</p>
       </div>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
+var hornedBeastsConstants = require('./constants/hornedBeastConstants');
 
 
 class Main extends React.Component {
   render() {
+    let beasts = [];
+    hornedBeastsConstants.forEach(item => {
+      beasts.push(<HornedBeasts name={item.title} title={item.title} imgUrl={item.image_url} description={item.description} />)
+    })
     return (
     <main>
-      <HornedBeasts imgUrl=""/>
-      <HornedBeasts imgUrl=""/>
+      {beasts}
     </main>)
   }
 }
