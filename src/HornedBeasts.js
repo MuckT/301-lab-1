5b5ca19e786ef4fef55f65b695f045ed7da274f3
MuckT <mcgtom10@gmail.com>
2021-03-22 22:24:00 -0700

    chore: add custom eslint and fix nits

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -6,10 +6,14 @@ class HornedBeasts extends React.Component {
     return (
       <div>
         <h2>{this.props.name}</h2>
-        <img title={this.props.name} alt={this.props.name + " image"} src={this.props.imgUrl} ></img>
+        <img
+          title={this.props.name}
+          alt={this.props.name + ' image'}
+          src={this.props.imgUrl}
+        ></img>
         <p>{this.props.description}</p>
       </div>
-    )
+    );
   }
 }
 
